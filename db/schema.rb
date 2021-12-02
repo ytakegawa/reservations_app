632583271f00f2183469133c222a3efc9ed04b9b
ytakegawa <ytkgw1986@gmail.com>
2021-12-02 15:49:25 +0900

    Add: Make a basic User model(including secure passwords)
    Userモデルの新規作成（セキュアなパスワード設定も追加）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_29_081416) do
+ActiveRecord::Schema.define(version: 2021_12_02_021941) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2021_10_29_081416) do
     t.integer "number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
